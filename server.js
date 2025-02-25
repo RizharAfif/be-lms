ffc88c5424ead021a2e65f4dfe68f8ebab052a43
John Doe <rizharafif11@gmail.com>
2025-02-25 11:59:49 +0700

    fix code

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => res.send("API WORKING"));
 app.post("/clerk", express.json(), clerkWebhooks);
 
 // Port
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
