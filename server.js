99452587c644579c404258bb1e1ea6d6cd141dc3
John Doe <rizharafif11@gmail.com>
2025-02-25 10:38:27 +0700

    connect db and make model User

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./configs/mongodb.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
 // Initialize express
 const app = express();
+
+// Connect database
+await connectDB();
 
 // Middleware
 app.use(cors());
 
 // Routes
 app.get("/", (req, res) => res.send("API WORKING"));
+app.post("/clerk", express.json(), clerkWebhooks);
 
 // Port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
